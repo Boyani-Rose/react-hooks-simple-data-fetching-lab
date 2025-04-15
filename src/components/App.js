0abaec2d35666a126e9d0bee97eb5aab0caefa83
Rose Momanyi <rosebmomanyi@gmail.com>
2025-04-15 14:35:09 +0300

    Implemented a fetch request inside useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import { useState, useEffect } from "react"
+function App(){
+    const [dogImage, setDogImage] = useState(null)
+    const [loading, setLoading] = useState(true)
+    
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((response)=>response.json())
+        .then((data)=>{
+            setDogImage(data.message)
+            setLoading(false)
+        })
+    },[dogImage])
+  
+        if (loading) {
+           return <p>Loading...</p>
+        }
+    return (
+        <img src="https://images.dog.ceo/breeds/bulldog-english/mami.jpg" alt="A Random Dog"/>
+    )
+
+}
+export default App
